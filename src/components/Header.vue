9b32369039d225a9a17f15b5c63987868370b779
arperevalov <lovetraindriver@gmail.com>
2024-04-13 21:57:36 +0500

    feat: Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <RouterLink to="/">
+            Home
+        </RouterLink>
+
+        <RouterLink to="/cocktail/margarita">
+            Margarita
+        </RouterLink>
+        <RouterLink to="/cocktail/mojito">
+            Mojito
+        </RouterLink>
+        <RouterLink to="/cocktail/a1">
+            A1
+        </RouterLink>
+        <RouterLink to="/cocktail/kir">
+            Kir
+        </RouterLink>
+        <RouterLink to="/404">
+            404
+        </RouterLink>
+    </header>
+</template>
+
+<style scoped>
+</style>
