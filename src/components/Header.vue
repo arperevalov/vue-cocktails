af935797075a1f7aa271bcb94eb2f45f9da76bd4
arperevalov <lovetraindriver@gmail.com>
2024-04-13 23:59:47 +0500

    fix: link class at header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,19 +3,19 @@
 
 <template>
     <header>
-        <RouterLink to="/cocktail/margarita" active-class="link-active">
+        <RouterLink to="/cocktail/margarita" class="link" active-class="active">
             Margarita
         </RouterLink>
-        <RouterLink to="/cocktail/mojito"  active-class="link-active">
+        <RouterLink to="/cocktail/mojito" class="link" active-class="active">
             Mojito
         </RouterLink>
-        <RouterLink to="/cocktail/a1"  active-class="link-active">
+        <RouterLink to="/cocktail/a1" class="link" active-class="active">
             A1
         </RouterLink>
-        <RouterLink to="/cocktail/kir"  active-class="link-active">
+        <RouterLink to="/cocktail/kir" class="link" active-class="active">
             Kir
         </RouterLink>
-        <RouterLink to="/404"  active-class="link-active">
+        <RouterLink to="/404" class="link" active-class="active">
             404
         </RouterLink>
     </header>
